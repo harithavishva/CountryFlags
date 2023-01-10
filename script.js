af80b1ce8c92e92a1a0a988463b86202a147301c
Haritha V <90097766+harithavishva@users.noreply.github.com>
2023-01-10 06:13:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    countryData.forEach((element)=>{
+        console.log(element.flag);
+    })}
